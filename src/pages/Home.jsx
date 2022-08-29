e7e04579888cd897c43b08434e3eaa9e644cf4c1
Cody Clements <subliminalverses2@gmail.com>
2022-08-28 20:19:47 -0500

    Style home page component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.hero_section}>
-      <h1>Its not about looks its about color!</h1>
+      <h1>Its not about looks its about comfort!</h1>
       <button className={styles.shop_now__btn}>Shop Now</button>
     </div>
   );
